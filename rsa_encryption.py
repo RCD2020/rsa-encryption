65b8a367bc80fa5fa121a2b1163251d18fed5cd9
RCD2020 <dobertravis@icloud.com>
2024-04-30 06:22:00 -0600

    added rsa encryption file

---
+++ rsa_encryption.py
@@ -0,0 +1,57 @@
+'''
+Apr 29, 2024
+'''
+
+from math_encrypted import (generate_prime, random_coprime,
+                            modular_inverse)
+
+
+def generate_keys(bit_length:int=128):
+    "Returns (public, private, modulus)"
+
+    # choose prime numbers, p and q
+    p, q = generate_prime(bit_length), generate_prime(bit_length)
+
+    # calculate our modulus, n
+    n = p * q
+
+    # find number of positive numbers coprime with n, phi
+    # we do this using Euler's Totient Function
+    # (p - 1) * (q - 1)
+    phi = (p - 1) * (q - 1)
+
+    # generate a public exponent for encryption, e
+    # check for random numbers that are coprime with phi
+    # that is to say share a greatest common divisor of 1
+    e = random_coprime(phi)
+
+    # generate a private exponent for decryption, d
+    # d is the modular multiplicative inverse, meaning that the
+    # modular exponentiation done with e is reversible using d
+    d = modular_inverse(e, phi)
+
+    return (e, d, n)
+
+
+def encrypt(data, public, modulus):
+    return [pow(num, public, modulus) for num in data]
+
+
+def encrypt_text(text, public, modulus):
+    return encrypt([ord(char) for char in text], public, modulus)
+
+
+def decrypt(data, private, modulus):
+    return [pow(num, private, modulus) for num in data]
+
+
+def decrypt_text(data, private, modulus):
+    return ''.join([chr(num) for num in decrypt(data, private, modulus)])
+
+
+if __name__ == '__main__':
+    public, private, modulus = generate_keys()
+    data = encrypt_text('test', public, modulus)
+    print(data)
+    print(decrypt_text(data, private, modulus))
+    
