a6774cc9c52f910f99781ddbe610f1212711de35
RCD2020 <dobertravis@icloud.com>
2024-05-19 16:58:15 -0600

    removed AJAX as I do not how to make it work

--- app.py
+++ app.py
@@ -16,17 +16,5 @@ async def index():
     return await render_template('index.html')
 
 
-@app.route('/get_keys', methods=['POST'])
-async def get_keys():
-    data = request.json
-    public, private, modulus = generate_keys(data['bit_length'])
-
-    return await jsonify({
-        'public': public,
-        'private': private,
-        'modulus': modulus
-    })
-
-
 if __name__ == '__main__':
     app.run(debug=True)
