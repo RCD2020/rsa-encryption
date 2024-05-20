9b318dd4d9692c557e9eeab08073988c3d78d219
RCD2020 <dobertravis@icloud.com>
2024-05-20 14:47:05 -0600

    added decryption to backend

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ Robert Davis
 '''
 
 from quart import Quart, render_template, request
+from json import loads as json_loads
 
-from rsa_encryption import generate_keys, encrypt_text
+from rsa_encryption import generate_keys, encrypt_text, decrypt_text
 
 
 app = Quart(__name__)
@@ -15,6 +16,7 @@ app = Quart(__name__)
 async def index():
     keys = ''
     encrypted_message = ''
+    decrypted_message = ''
 
     if request.method == 'POST' and (await request.form)['type'] == 'keys':
         bit_length = int((await request.form)['bit_length'])
@@ -34,8 +36,20 @@ async def index():
             message, public, modulus
         )
 
+    if request.method == 'POST' and (await request.form)['type'] == 'decrypt':
+        encrypted: str = (await request.form)['encrypted']
+        private = int((await request.form)['private_key'])
+        modulus = int((await request.form)['modulus'])
+
+        encrypted = [int(x) for x in encrypted.strip('[]').split(', ')]
+
+        decrypted_message = decrypt_text(
+            encrypted, private, modulus
+        )
+
     return await render_template(
-        'index.html', keys=keys, encrypted_message=encrypted_message
+        'index.html', keys=keys, encrypted_message=encrypted_message,
+        decrypted_message=decrypted_message
     )
 
 
