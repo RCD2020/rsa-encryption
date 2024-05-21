4168183c855c24b5f6ae7aad042f16708e122fe3
RCD2020 <dobertravis@icloud.com>
2024-05-21 11:14:46 -0600

    allows for persisting fields

--- app.py
+++ app.py
@@ -14,42 +14,44 @@ app = Quart(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 async def index():
-    keys = ''
-    encrypted_message = ''
-    decrypted_message = ''
+    public, private, modulus = [''] * 3
+    message, encrypted, encrypted_message, decrypted_message = [''] * 4
+    bit_length = 32
 
-    if request.method == 'POST' and (await request.form)['type'] == 'keys':
-        bit_length = int((await request.form)['bit_length'])
+    if request.method == 'POST':
+        form = await request.form
 
-        public, private, modulus = generate_keys(bit_length)
-        keys = {
-            'public': (public, modulus),
-            'private': (private, modulus)
-        }
+        public = int(form['public']) if form.get('public') else ''
+        private = int(form['private']) if form.get('private') else ''
+        modulus = int(form['modulus']) if form.get('modulus') else ''
+        message = form['message'] if form.get('message') else ''
+        encrypted = form['encrypted'] if form.get('encrypted') else ''
+        encrypted_message = form['encrypted_message'] if form.get('encrypted_message') else ''
+        decrypted_message = form['decrypted_message'] if form.get('decrypted_message') else ''
+        bit_length = int(form['bit_length']) if form.get('bit_length') else bit_length
+
+        post_type = form.get('type')
+
+        if post_type == 'keys':
+            public, private, modulus = generate_keys(bit_length)
     
-    if request.method == 'POST' and (await request.form)['type'] == 'encrypt':
-        message = (await request.form)['message']
-        public = int((await request.form)['public_key'])
-        modulus = int((await request.form)['modulus'])
+        if post_type == 'encrypt':
+            encrypted_message = encrypt_text(
+                message, public, modulus
+            )
 
-        encrypted_message = encrypt_text(
-            message, public, modulus
-        )
+        if post_type == 'decrypt':
+            encrypted = [int(x) for x in encrypted.strip('[]').split(', ')]
 
-    if request.method == 'POST' and (await request.form)['type'] == 'decrypt':
-        encrypted: str = (await request.form)['encrypted']
-        private = int((await request.form)['private_key'])
-        modulus = int((await request.form)['modulus'])
-
-        encrypted = [int(x) for x in encrypted.strip('[]').split(', ')]
-
-        decrypted_message = decrypt_text(
-            encrypted, private, modulus
-        )
+            decrypted_message = decrypt_text(
+                encrypted, private, modulus
+            )
 
     return await render_template(
-        'index.html', keys=keys, encrypted_message=encrypted_message,
-        decrypted_message=decrypted_message
+        'index.html', encrypted_message=encrypted_message,
+        decrypted_message=decrypted_message, public=public,
+        private=private, modulus=modulus, bit_length=bit_length,
+        message=message, encrypted=encrypted
     )
 
 
